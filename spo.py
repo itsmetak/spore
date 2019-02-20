f3b5867e1230c39c2daa41526b21559483c6f333
itsmetak <davidetkcs@gmail.com>
2019-02-20 18:34:45 +0100

    If playlist exitst append to original playlist

--- spo.py
+++ spo.py
@@ -14,6 +14,7 @@ def loginSpotify(creds):
     return sp
 
 def cleanTrackList(tracklist):
+
     tracklist = [ track.split('[')[0] for track in tracklist ]
     tracklist = [ re.sub(r"[^a-zA-Z0-9 ]","",track) for track in tracklist ]
 
@@ -21,8 +22,14 @@ def cleanTrackList(tracklist):
     
 
 def createPlaylist(sp, name):
-    plist = sp.user_playlist_create('davidtkcs', name)
-    return plist
+
+    playlists = sp.user_playlists('davidtkcs')
+    ids = [item['name'] for item in playlists['items']]
+    if name in ids:
+        idlist = ids.index(name)
+        return playlists['items'][idlist]
+    else:
+        return sp.user_playlist_create('davidtkcs', name)
 
 def addSongsToPlaylist(sp, plist, tracklist):
     
@@ -43,5 +50,5 @@ def addSongsToPlaylist(sp, plist, tracklist):
 if __name__ == '__main__':
     l = ['Harvey Danger - Flagpole Sitta [Alternative]', 'Depeche Mode - Enjoy The Silence [Synthpop]', 'Peter Bjorn and John - Young Folks [Indie Pop]', 'The Weight - The Band [Folk]', 'Chino Cappin - Slime [Hip-Hop]', 'Mobb Deep - Shook Ones [Hip-Hop]', 'Stevie Wonder - Superstition [R&B]', 'Robert Plant & Alison Krauss - Fortune Teller [ rock folk ]', 'Dead Kennedys - Pull My Strings [Punk Rock]', 'RX Bandits - Apparition - Audiotree Live [Prog Rock]', "DVDA - Now You're a Man [Rock/South Park]"]
     sp = loginSpotify('spotcreds.txt')
-    pl = createPlaylist('test')
-    addSongsToPlaylist(pl,l)
+    pl = createPlaylist(sp, 'test')
+    addSongsToPlaylist(sp,pl,l)
